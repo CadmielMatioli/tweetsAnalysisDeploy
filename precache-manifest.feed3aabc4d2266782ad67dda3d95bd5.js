611031aeb9a176bd2a68bcefc1f4de83255d7033
cadmiel <cadmatioli@gmail.com>
2020-07-03 10:39:48 -0300

    Updates

---
+++ precache-manifest.feed3aabc4d2266782ad67dda3d95bd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7e83220e4c98c08aa22648341e7296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "723a73dfe17fa5c5814e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "083289debcaea6810a45",
+    "url": "/static/css/main.c0e10030.chunk.css"
+  },
+  {
+    "revision": "723a73dfe17fa5c5814e",
+    "url": "/static/js/2.f3d83cfe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f3d83cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "083289debcaea6810a45",
+    "url": "/static/js/main.9619ea24.chunk.js"
+  },
+  {
+    "revision": "648dfad5e0063bdc3a98",
+    "url": "/static/js/runtime-main.3ef713f4.js"
+  },
+  {
+    "revision": "538682a54b3d3e1575814784425292ad",
+    "url": "/static/media/sentiment.538682a5.jpg"
+  }
+]);
